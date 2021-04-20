1686a098c96f5fde9deefcb5fd1a34798303b549
swiing <persovince@yahoo.fr>
2021-04-20 10:19:24 +0200

    add typescript type declaration file

---
+++ dist/bit-typedarray.d.ts
@@ -0,0 +1,38 @@
+/**
+ *  A BitArray object exhibiting the interface of standard ecmascript TypedArray's
+ *  cf. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
+ *
+ *  @author swiing
+ */
+declare type bit = 0 | 1;
+declare class BitArray {
+    buffer: ArrayBuffer;
+    byteLength: number;
+    byteOffset: number;
+    length: number;
+    prototype: object;
+    static BYTES_PER_ELEMENT: number;
+    static from(source: any[]): BitArray;
+    static of(...items: any[]): BitArray;
+    /**
+     * At this stage, only the ( length ) signature is supported.
+     * @todo: other signatures - cf. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
+     * @param length
+     * @returns a proxy instance
+     */
+    constructor(length: number);
+    toString(): string;
+    forEach<T>(callback: (value: bit, index: number, thisArg?: T) => any, thisArg?: this | T): void;
+    at(index: number): any;
+    set(source: bit[], offset?: number): void;
+    values(): {
+        next: () => {
+            done: boolean;
+            value: any;
+        } | {
+            done: boolean;
+            value?: undefined;
+        };
+    };
+}
+export default BitArray;
